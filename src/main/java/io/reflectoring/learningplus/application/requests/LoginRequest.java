92bffaa8c24a445b9205878b4a5e72ea984d6449
harini-lenora <lenoraharini12@gmail.com>
2025-04-26 22:39:59 +0530

    add loginReq

---
+++ src/main/java/io/reflectoring/learningplus/application/requests/LoginRequest.java
@@ -0,0 +1,13 @@
+package io.reflectoring.learningplus.application.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
