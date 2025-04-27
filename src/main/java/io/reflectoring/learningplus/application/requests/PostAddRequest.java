35212799508931ab54d9bfa07acbc89ea026ebb2
yasasArt <yasasjayaweera945@gmail.com>
2025-04-27 13:21:37 +0530

    Post Controller, services

---
+++ src/main/java/io/reflectoring/learningplus/application/requests/PostAddRequest.java
@@ -0,0 +1,13 @@
+package io.reflectoring.learningplus.application.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostAddRequest {
+    private int userId;
+    private String Description;
+}
